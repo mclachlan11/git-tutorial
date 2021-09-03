3c85c7621c686f0299aea80e39e04d910c7a9846
Adam Lis <adambricelis@gmail.com>
2021-09-03 11:40:01 -0400

    Remove HTML editing from tutorial

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    var people = document.getElementsByClassName("person-frame-inner");
-    for (let person of people) {
-        person.style["animation-delay"] = -Math.floor(Math.random() * 1000) + "s"
+$.ajax({
+    url: "images/",
+    success: data => {
+        $(data).find("a").attr("href", (_i, filename) => {
+            if (filename.match(/\.(jpe?g|png|gif)$/)) {
+                $('#heads-area').append(
+                    "<div class=\"person-frame-inner\"><img class=\"ball\" src=\"images/" + filename + "\"/></div>"
+                );
+            }
+        });
+
+        let people = document.getElementsByClassName("person-frame-inner");
+        for (let person of people) {
+            person.style["animation-delay"] = -Math.floor(Math.random() * 1000) + "s"
+        }
     }
 });
